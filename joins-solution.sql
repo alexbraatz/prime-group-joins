b037d691392342e1e7e540683a732ad9b49bf5cf
alex braatz <alexjbraatz@gmail.com>
2021-04-06 11:58:52 -0500

    continue challenge

--- joins-solution.sql
+++ joins-solution.sql
@@ -36,6 +36,13 @@ JOIN products ON products.id = warehouse_product.product_id
 WHERE products.id = 5;
 ```
 4. Which warehouses have diet pepsi?
+```
+SELECT *
+FROM warehouse
+JOIN warehouse_product ON warehouse.id = warehouse_product.warehouse_id
+JOIN products ON products.id = warehouse_product.product_id
+WHERE products.id = 6
+```
 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 6. How many customers do we have?
 7. How many products do we carry?
